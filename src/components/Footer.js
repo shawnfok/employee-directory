fde55a1ab6db421fa36d18ba6310832d16c5c5bd
shawnfok <shawnfok@gmail.com>
2020-12-29 18:25:24 -0500

    created header and footer files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="m-10 text-sm text-center text-gray-500">
+            <p>Created by <a href="https://github.com/shawnfok" className="font-bold text-gray-600 hover:text-yellow-500">Shawn Fok</a></p>
+        </div>
+    )
+}
+
+export default Footer;
